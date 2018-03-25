78800c454ddc32af8e7167f936fa87ca326ec731
Sean Donnelly <sdonne5@students.towson.edu>
2018-03-25 00:28:37 -0400

    Add TypeScript and SCSS support

---
+++ src/index.tsx
@@ -0,0 +1,15 @@
+// Be sure to use the 'import * as' syntax for typescript
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+// SCSS support is built in, all you have to do is import the file
+import './styles.scss';
+
+/**
+ * Welcome! To get started, replace the h1 tag below with your own
+ * custom React component.
+ */
+ReactDOM.render(
+  <h1>Make something great!</h1>,
+  document.getElementById('root')
+);
